0054296223c09a94aad27aba66617ac98a857f40
mezdahun <mezeydavid@gmail.com>
2023-05-05 14:41:58 +0200

    some random model from roboflow

--- cobe/settings/odmodel.py
+++ cobe/settings/odmodel.py
@@ -2,8 +2,8 @@
 # api_key to be able to download model from roboflow server
 api_key = "api_key"
 # model name
-model_name = "default"
-# url of the background roboflow inference server container
-inf_server_url = "http://localhost:9001"
+model_name = "projekt_gesty"
+# url of the background roboflow inference server container (with ending /)
+inf_server_url = "http://localhost:9001/"
 model_id = "/" + model_name
-version = "1"
+version = "2"
