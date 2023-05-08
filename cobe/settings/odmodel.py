38f276443659e9b7a2a1cf4367d518e9aa027d66
mezdahun <mezeydavid@gmail.com>
2023-05-08 11:50:07 +0200

    annotation fix

--- cobe/settings/odmodel.py
+++ cobe/settings/odmodel.py
@@ -1,9 +1,16 @@
 """Settings of the object detection model trained on roboflow server"""
 # api_key to be able to download model from roboflow server
-api_key = "api_key"
+api_key = "gX0Zo9eq9sRf7wfJNDMH"
+# # model name
+# model_name = "projekt_gesty"
+# # url of the background roboflow inference server container (with ending /)
+# inf_server_url = "http://localhost:9001/"
+# model_id = "/" + model_name
+# version = "2"
+
 # model name
-model_name = "projekt_gesty"
+model_name = "face-detection-mik1i"
 # url of the background roboflow inference server container (with ending /)
 inf_server_url = "http://localhost:9001/"
 model_id = "/" + model_name
-version = "2"
+version = "7"
