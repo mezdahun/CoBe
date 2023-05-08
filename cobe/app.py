599103cf48e13bc0d7f5db2dc6d1a37c4cbd666c
mezdahun <mezeydavid@gmail.com>
2023-05-08 14:50:20 +0200

    add cleanup and shutdown entrypoints

--- cobe/app.py
+++ cobe/app.py
@@ -3,3 +3,13 @@ from cobe.cobe.cobemaster import CoBeMaster
 def main():
     master = CoBeMaster()
     master.start()
+
+def cleanup_inf_servers():
+    """Cleans up the inference servers on all eyes"""
+    master = CoBeMaster()
+    master.cleanup_inference_servers()
+
+def shutdown_eyes():
+    """Shuts down all eyes"""
+    master = CoBeMaster()
+    master.shutdown_eyes()
