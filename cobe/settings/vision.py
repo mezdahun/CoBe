a42bba6e1bd623180afa113cdf70fa3d3bf0f17f
mezdahun <mezeydavid@gmail.com>
2023-06-11 13:00:04 +0200

    try display and capture height-width

--- cobe/settings/vision.py
+++ cobe/settings/vision.py
@@ -3,8 +3,10 @@ import os
 
 ### Camera stream settings ###
 flip_method = os.getenv("FLIP_METHOD", 2)
-capture_width = os.getenv("CAPTURE_WIDTH", 1280)  # fisheye calibration maps should be adjusted to this resolution
-capture_height = os.getenv("CAPTURE_HEIGHT", 720)  # fisheye calibration maps should be adjusted to this resolution
+capture_width = os.getenv("CAPTURE_WIDTH", 1640)  # fisheye calibration maps should be adjusted to this resolution
+capture_height = os.getenv("CAPTURE_HEIGHT", 1232)  # fisheye calibration maps should be adjusted to this resolution
+display_width = os.getenv("DISPLAY_WIDTH", 820)
+display_height = os.getenv("DISPLAY_HEIGHT", 616)
 frame_rate = os.getenv("FRAME_RATE", 5)
 
 ### Published MJPEG stream settings ###
