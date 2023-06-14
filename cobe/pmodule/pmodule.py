852be55ba356112c3a21b2214bf89ada0845fc9c
mezdahun <mezeydavid@gmail.com>
2023-06-14 08:30:10 +0200

    settings refactored

--- cobe/pmodule/pmodule.py
+++ cobe/pmodule/pmodule.py
@@ -2,6 +2,7 @@ import os
 from time import sleep
 from cobe.tools.filetools import is_process_running, clear_directory
 import subprocess
+import cobe.settings.pmodulesettings as ps
 
 root_folder = "C:\\Users\\David\\Documents\\predprey_batches"
 docker_path = "C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe"
@@ -24,22 +25,31 @@ def entry_start_docker_container(batch_size=4, num_prey=50):
 
     if not is_process_running("Docker Desktop.exe"):
         print("Launching Docker")
-        subprocess.Popen(docker_path)
-        sleep(10)
+        subprocess.Popen(ps.docker_path)
+        sleep(ps.docker_startup_timeout)
     else:
         print("Docker already running")
 
     print("Loading P-module docker image")
     try:
         # First attempt to clear any image that exists by the same name
-        os.system(f'cmd /c "docker image rm predpreyoriginal:latest"')
+        os.system(f'cmd /c "docker image rm {ps.docker_image_name}"')
     except:
+        print("Couldn't remove image, probably doesn't exist [Pmodule:entry_start_docker_container]]")
         pass
 
-    os.system(f'cmd /c "docker load -i {root_folder}\\docker.tar"')
+    tar_path = os.path.join(ps.root_folder, "docker.tar")
+    if os.path.isfile(tar_path):
+        os.system(f'cmd /c "docker load -i {tar_path}"')
+    else:
+        raise Exception("Docker image not found under path: " + tar_path)
 
     print("Starting P-module docker container")
-    os.system(f'cmd /c "docker run --name cont -v {root_folder}://usr/src/myapp/batches -t -d predpreyoriginal:latest  sh run.sh -b {batch_size} -n {num_prey}"')
+    os.system(
+        f'cmd /c "docker run --name cont '
+        f'-v {ps.root_folder}:{ps.root_folder_on_container} '
+        f'-t -d {ps.docker_image_name} sh run.sh -b {batch_size} -n {num_prey}"')
+
 
 def entry_cleanup_docker_container():
     """Stops and removes the docker container of the Pmodule via command line"""
