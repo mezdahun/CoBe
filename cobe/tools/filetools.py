7e7aef89eb0bdef1243e1e2976c1aa2b01c54778
David James <david.james.dj551@gmail.com>
2023-06-20 17:51:31 +0200

    "Added section to pmodule to handle multiple start-up calls. Added error handling to filetools.py to handle PID ProcessLookupError. Added new samplerun.py file to demonstrate usage of combined pmodule and renderingstack"

--- cobe/tools/filetools.py
+++ cobe/tools/filetools.py
@@ -5,10 +5,25 @@ import os
 def is_process_running(name: str) -> bool:
     """Checks if a process is running on the system by comparing the name of each running process"""
     for pid in psutil.pids():
-        p = psutil.Process(pid)
-        if p.name() == name:
-            # process found
-            return True
+        try:
+            p = psutil.Process(pid)
+            if p.name() == name:
+                # process found
+                return True
+        except ProcessLookupError as e:
+            print(f"ProcessLookupError while checking PIDs; {e}")
+            print("This error needs further investigation")
+
+            next
+
+            # It seems like in some edge cases the PID can terminate between identifying the PID 
+            # and calling its name via p.name(). In such cases we should just continue to evaluate 
+            # the rest of the list.
+        except Exception as e:
+            print(f"Unexpected error of type {type(e)} while checking PIDs; {e}")
+
+            
+
     return False
 
 def clear_directory(file_path: str):
