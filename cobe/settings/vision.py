c70d522bcdddf7cffd3c939e9565f5674fb67d51
mezdahun <mezeydavid@gmail.com>
2023-07-07 10:37:15 +0200

    parametrizing zoom parameters

--- cobe/settings/vision.py
+++ cobe/settings/vision.py
@@ -2,14 +2,20 @@
 import os
 
 ### Camera stream settings ###
+# The stream is taken according to the available sensor modes with capture_width and capture_height resolution
+# on framerate frame_rate. The stream is then cropped to crop_width and crop_height starting from start_x and
+# start_y. The cropped image is then resized to display_width and display_height. The resized image is then
+# flipped according to flip_method.
+
 flip_method = os.getenv("FLIP_METHOD", 2)
 capture_width = os.getenv("CAPTURE_WIDTH", 3264)
 capture_height = os.getenv("CAPTURE_HEIGHT", 2464)
-
-display_width = os.getenv("DISPLAY_WIDTH", 3000)  # fisheye calibration maps should be adjusted to this resolution
-display_height = os.getenv("DISPLAY_HEIGHT", 2000)  # fisheye calibration maps should be adjusted to this resolution
 start_x = os.getenv("START_X", 0)  # start cropping from this x coordinate for display_width length
 start_y = os.getenv("START_Y", 0)  # start cropping from this y coordinate for display_height length
+crop_width = os.getenv("CROP_WIDTH", 3264)  # crop the image to this width
+crop_height = os.getenv("CROP_HEIGHT", 2464)  # crop the image to this height
+display_width = os.getenv("DISPLAY_WIDTH", 3264)  # fisheye calibration maps should be adjusted to this resolution
+display_height = os.getenv("DISPLAY_HEIGHT", 2464)  # fisheye calibration maps should be adjusted to this resolution
 
 end_x = min(start_x + display_width, capture_width)  # end cropping at this x coordinate
 end_y = min(start_y + display_height, capture_height)  # end cropping at this y coordinate
