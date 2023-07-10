db1d21a43c06460e0eeab2e744b6a2ed209c47ec
David James <david.james.dj551@gmail.com>
2023-07-10 11:06:14 +0200

    add IP for test setup

--- cobe/settings/network.py
+++ cobe/settings/network.py
@@ -4,7 +4,7 @@ parameters for the Pyro5 local network
 eyes = {
     "eye_0": {
         "expected_id": 0,
-        "host": "192.168.100.142",
+        "host": "192.168.0.101",  # "192.168.100.142",
         "port": "1234",
         "uri": "PYRO:",
         "name": "cobe.eye",
