c43f9b681cedb6d236ba53ccb7e15937996681a9
David James <david.james.dj551@gmail.com>
2023-07-10 15:16:36 +0200

    change vision parameers before training data collection

--- cobe/settings/vision.py
+++ cobe/settings/vision.py
@@ -10,13 +10,13 @@ import os
 flip_method = os.getenv("FLIP_METHOD", 2)
 capture_width = os.getenv("CAPTURE_WIDTH", 3264)
 capture_height = os.getenv("CAPTURE_HEIGHT", 2464)
-start_x = os.getenv("START_X", 1100)  # start cropping from this x coordinate for display_width length
-start_y = os.getenv("START_Y", 800)  # start cropping from this y coordinate for display_height length
-crop_width = os.getenv("CROP_WIDTH", 1400)  # crop the image to this width
-crop_height = os.getenv("CROP_HEIGHT", 1200)  # crop the image to this height
+start_x = os.getenv("START_X", 1300)  # start cropping from this x coordinate for display_width length
+start_y = os.getenv("START_Y", 1000)  # start cropping from this y coordinate for display_height length
+crop_width = os.getenv("CROP_WIDTH", 600)  # crop the image to this width
+crop_height = os.getenv("CROP_HEIGHT", 600)  # crop the image to this height
 
-display_width = os.getenv("DISPLAY_WIDTH", 800)  # fisheye calibration maps should be adjusted to this resolution
-display_height = os.getenv("DISPLAY_HEIGHT", 700)  # fisheye calibration maps should be adjusted to this resolution
+display_width = os.getenv("DISPLAY_WIDTH", 416)  # fisheye calibration maps should be adjusted to this resolution
+display_height = os.getenv("DISPLAY_HEIGHT", 416)  # fisheye calibration maps should be adjusted to this resolution
 
 end_x = min(start_x + crop_width, capture_width)  # end cropping at this x coordinate
 end_y = min(start_y + crop_height, capture_height)  # end cropping at this y coordinate
