48277927a21c7f293e9a5b8aa2a80a4b1e3b5527
David James <david.james.dj551@gmail.com>
2023-08-02 11:52:54 +0200

    cleanup rendering settings

--- cobe/settings/rendersettings.py
+++ cobe/settings/rendersettings.py
@@ -1,11 +1,22 @@
 # Path to the Unity application (Only implemented on Windows)
-unity_path = 'C:\\Users\\David\\Desktop\\Test-Build\\CoBe.exe'
-resolume_path = "C:\\Program Files\\Resolume Arena\\Arena.exe"
-image_file_path = "C:\\Users\\David\\Pictures\\4kcalib.jpg" # anticipating 4k resolution: 3840 x 3840 pixels
+import os
+
+# todo: Create a unified project folder instead of this scattered system
+# Unity parameters
+unity_app_executable_name = "CoBe.exe"
+unity_app_path = "C:\\Users\\David\\Desktop\\Test-Build"
+
+# Resolume parameters
+resolume_app_executable_name = "Arena.exe"
+resolume_path = "C:\\Program Files\\Resolume Arena"
+
+# Deducted paths
+unity_path = os.path.join(unity_app_path, unity_app_executable_name)
+resolume_path = os.path.join(resolume_path, resolume_app_executable_name)
 
 # TCP info
-ip_address = "127.0.0.1"
-port = 13000
+ip_address = "127.0.0.1"  # IP for TCP connection to listen on
+port = 13000              # Port for TCP connection to listen on
 
 # Other
 start_up_delay = 3
