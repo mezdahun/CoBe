92c8a5b146d64eadd5387c836fc5b684a191e3e3
David James <david.james.dj551@gmail.com>
2023-08-14 16:48:32 +0200

    add ssh parameters for fabric

--- cobe/settings/network.py
+++ cobe/settings/network.py
@@ -1,11 +1,14 @@
 """
 parameters for the Pyro5 local network
 """
+nano_username = "nano"
+nano_cobe_installdir = "/home/nano/Desktop/CoBe"
+unified_eyeserver_port = 1234
 eyes = {
     "eye_0": {
         "expected_id": 0,
         "host": "192.168.0.102",
-        "port": "1234",
+        "port": f"{unified_eyeserver_port}",
         "uri": "PYRO:",
         "name": "cobe.eye",
         "fisheye_calibration_map": "map_eye_0.npz"
