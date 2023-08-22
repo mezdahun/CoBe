0b427f8146ebdb5c13e96949cd80453cfae29653
mezdahun <mezeydavid@gmail.com>
2023-08-22 10:45:02 +0200

    remove broken dependencies

--- cobe/kalmanprocess/kalmanprocess.py
+++ cobe/kalmanprocess/kalmanprocess.py
@@ -3,8 +3,8 @@
 import numpy as np
 import logging
 # from filterpy.kalman import KalmanFilter
-from scipy.linalg import block_diag
-from filterpy.common import Q_discrete_white_noise
+# from scipy.linalg import block_diag
+# from filterpy.common import Q_discrete_white_noise
 
 from cobe.pmodule.pmodule import generate_pred_json
 from cobe.settings import logs
