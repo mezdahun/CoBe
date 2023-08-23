87f7f199b91a17f18523d0e7b36a4ab7d04ae744
mezdahun <mezeydavid@gmail.com>
2023-08-23 09:57:25 +0200

    prepare for multi-eye system

--- cobe/settings/network.py
+++ cobe/settings/network.py
@@ -4,6 +4,7 @@ parameters for the Pyro5 local network
 nano_username = "nano"
 nano_cobe_installdir = "/home/nano/Desktop/CoBe"
 unified_eyeserver_port = 1234
+
 eyes = {
     "eye_0": {
         "expected_id": 0,
@@ -12,5 +13,13 @@ eyes = {
         "uri": "PYRO:",
         "name": "cobe.eye",
         "fisheye_calibration_map": "map_eye_0.npz"
-    }
+    },
+    # "eye_1": {
+    #     "expected_id": 1,
+    #     "host": "192.168.0.103",
+    #     "port": f"{unified_eyeserver_port}",
+    #     "uri": "PYRO:",
+    #     "name": "cobe.eye",
+    #     "fisheye_calibration_map": "map_eye_1.npz"
+    # }
 }
