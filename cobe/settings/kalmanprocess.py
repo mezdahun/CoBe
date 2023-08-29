25a9e5d2582d592a8dbd00d252ee8cca41d6c6f5
David Mezey <mezeydavid@gmail.com>
2023-08-29 14:22:15 +0200

    fix kalman filter

--- cobe/settings/kalmanprocess.py
+++ cobe/settings/kalmanprocess.py
@@ -1,4 +1,4 @@
 #### Seeting for the Kalman filter that smooths the data and provides a fixed speed predator detection
-process_freq = 20  # frequency of process in Hz
-process_noise_var = 15  # variance of process noise
-measurement_noise_var = 0.65  # variance of measurement noise (in simulation space 20 x 20)
+process_freq = 40  # frequency of process in Hz
+process_noise_var = 13.5  # variance of process noise
+measurement_noise_var = 0.0005  # variance of measurement noise (in simulation space 20 x 20)
