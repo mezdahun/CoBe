97fc5e56d769691b0ffc76772571b1d25fdbd42e
mezdahun <mezeydavid@gmail.com>
2023-09-14 14:43:53 +0200

    fix directions

--- cobe/thymio/thymio.py
+++ cobe/thymio/thymio.py
@@ -186,6 +186,7 @@ class CoBeThymio(object):
         if self.left != self.right:
             # if not, set them to the same value
             self.left = self.right = (self.left + self.right) / 2
+            self.move()
 
 
 
