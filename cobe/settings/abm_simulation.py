4c42405f138c91d57006dbd78752769da6584d02
David <mezeydavid@gmail.com>
2024-01-17 11:31:30 +0100

    add abm parameters

--- cobe/settings/abm_simulation.py
+++ cobe/settings/abm_simulation.py
@@ -10,3 +10,13 @@ WITH_ABM = bool(int(os.getenv("WITH_ABM", 0)))
 # App version of the ABM module to be used, can be Base, CoopSig or VisualFlocking
 # Status: Base in progress, CoopSig is not working, VisualFlocking is not working
 ABM_APP_VERSION = os.getenv("ABM_APP_VERSION", "Base")
+
+
+# Max number of detectable agents in the scene
+MAX_NUM_AGENTS = int(os.getenv("MAX_NUM_AGENTS", 10))
+
+# Filename of agent lists
+AGENT_LIST_FILENAME = os.getenv("AGENT_LIST_FILENAME", "agent_list.json")
+
+# Arena size (always square)
+max_abs_coord = int(os.getenv("MAX_ABS_COORD", 500))
