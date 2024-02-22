c8a122b87c7cde5f7b7af56ff0a6745173d9ec7a
David Mezey <mezeydavid@gmail.com>
2024-02-22 09:49:24 +0100

    cleanup app

--- cobe/app.py
+++ cobe/app.py
@@ -29,11 +29,13 @@ def test_stream():
 
 def collect_pngs(eye_id=0):
     """Collects pngs from all eyes"""
-    args = argparse.ArgumentParser(description="Starts raw video stream and collects pngs from all or only selected eyes")
+    args = argparse.ArgumentParser(
+        description="Starts raw video stream and collects pngs from all or only selected eyes")
 
     # adding optional arguments
-    aid = args.add_argument("--eye_id", default=None, help="ID (int) of the eye (nano board) to start stream on accordin to"
-                                                           "settings.network")
+    aid = args.add_argument("--eye_id", default=None,
+                            help="ID (int) of the eye (nano board) to start stream on accordin to"
+                                 "settings.network")
     args = args.parse_args()
 
     if args.eye_id is not None:
@@ -54,8 +56,9 @@ def main(eye_id=0):
     args = argparse.ArgumentParser(description="Starts the the whole stack using a single eye.")
 
     # adding optional arguments
-    aid = args.add_argument("--eye_id", default=None, help="ID (int) of the eye (nano board) to start eyeserver on as in"
-                                                           "settings.network")
+    aid = args.add_argument("--eye_id", default=None,
+                            help="ID (int) of the eye (nano board) to start eyeserver on as in"
+                                 "settings.network")
     args = args.parse_args()
     if args.eye_id is not None:
         eye_ids = [eye['expected_id'] for eye in network.eyes.values()]
@@ -73,7 +76,7 @@ def main_kalman():
     # Creating queue to push real detection coordinates
     od_to_kalman_queue = Queue()
     # Creating process to run kalman filter in different thread
-    kalman_process = Process(target=kalman_process_OD, args=(od_to_kalman_queue, None, ))
+    kalman_process = Process(target=kalman_process_OD, args=(od_to_kalman_queue, None,))
     # Starting kalman process
     kalman_process.start()
     # Starting cobe master and passing shared queue
@@ -100,7 +103,8 @@ def start_database():
     # check if input folder is empty
     if len(os.listdir(database.database_input_path)) != 0:
         logger.warning(f"Database input folder {database.database_input_path} is not empty. ")
-        with_wipe_input = input("Do you want to wipe the input folder and only record data from now on? (y/n)").lower() == 'y'
+        with_wipe_input = input(
+            "Do you want to wipe the input folder and only record data from now on? (y/n)").lower() == 'y'
     else:
         with_wipe_input = False
 
@@ -154,7 +158,7 @@ def main_multieye_kalman():
     if pmodulesettings.num_predators == 1:
         kalman_process = Process(target=kalman_process_OD, args=(pred_queue, None,))
     else:
-        kalman_process = Process(target=kalman_process_OD_multipred, args=(pred_queue, None, ))
+        kalman_process = Process(target=kalman_process_OD_multipred, args=(pred_queue, None,))
 
     # Creating process to run different eyes in different threads
     master = CoBeMaster(pswd=pswd)
@@ -188,6 +192,7 @@ def main_multieye_kalman():
     kalman_process.join()
     logger.info("CoBe stopped. Bye!")
 
+
 def main_multieye():
     logger.info("Starting CoBe with multi-eye mode (no Kalman-filtering).")
 
@@ -216,7 +221,7 @@ def main_multieye():
     # Creating common queue to push detections
     pred_queue = Queue()
     # Creating kalman process to run in different thread
-    kalman_process = Process(target=file_writer_process, args=(pred_queue, ))
+    kalman_process = Process(target=file_writer_process, args=(pred_queue,))
 
     # Creating process to run different eyes in different threads
     master = CoBeMaster(pswd=pswd)
@@ -278,8 +283,9 @@ def start_eyeserver_silent():
     args = argparse.ArgumentParser(description="Starts the pyro5 eye servers on the chosen eyes")
 
     # adding optional arguments
-    aid = args.add_argument("--eye_id", default=None, help="ID (int) of the eye (nano board) to start eyeserver on as in"
-                                                           "settings.network")
+    aid = args.add_argument("--eye_id", default=None,
+                            help="ID (int) of the eye (nano board) to start eyeserver on as in"
+                                 "settings.network")
     args = args.parse_args()
 
     if args.eye_id is not None:
@@ -368,8 +374,9 @@ def start_eyeserver(eye_id=None):
     args = argparse.ArgumentParser(description="Starts the pyro5 eye servers on the chosen eyes")
 
     # adding optional arguments
-    aid = args.add_argument("--eye_id", default=None, help="ID (int) of the eye (nano board) to start eyeserver on as in"
-                                                           "settings.network")
+    aid = args.add_argument("--eye_id", default=None,
+                            help="ID (int) of the eye (nano board) to start eyeserver on as in"
+                                 "settings.network")
     args = args.parse_args()
 
     if args.eye_id is not None:
@@ -483,6 +490,7 @@ def start_eyeserver(eye_id=None):
     else:
         logger.info("No eyeservers started, exiting...")
 
+
 def stop_eyeserver():
     """Stops the pyro eyeserver on the eyes defined by settins.network via fabric. Can be used when
     eyeserver keeps running in the background blocking new connections"""
@@ -523,8 +531,9 @@ def thymio_remote_control(thymio_id=0):
     args = argparse.ArgumentParser(description="Starts the the whole stack using a single eye.")
 
     # adding optional arguments
-    aid = args.add_argument("--thymio_id", default=None, help="ID (int) of the thymio (nano board) to start thymioserver on as in"
-                                                           "settings.network")
+    aid = args.add_argument("--thymio_id", default=None,
+                            help="ID (int) of the thymio (nano board) to start thymioserver on as in"
+                                 "settings.network")
     args = args.parse_args()
     if args.thymio_id is not None:
         thymio_ids = [thymio['expected_id'] for thymio in network.thymios.values()]
@@ -535,71 +544,80 @@ def thymio_remote_control(thymio_id=0):
     if f"thymio_{thymio_id}" not in network.thymios.keys():
         raise ValueError(f"Thymio ID {thymio_id} not found in settings.network")
 
-
     thymio_controller = CoBeThymioMaster(target_thymio_name=f"thymio_{thymio_id}")
     thymio_controller.start_remote_control()
 
-def rand_filler_proc(output_queue, start_position=np.array([0, 0])):
-    """Random filler process to test queue-based processes"""
-    prev_position = start_position
-    while True:
-        random_position = np.array([random.randint(-20, 20)/20, random.randint(-20, 20)/20])
-        new_position = prev_position + random_position
-        output_queue.put([new_position])
-        print(f"putting {new_position} into queue")
-        prev_position = new_position
-        time.sleep(0.1)
 
 def thymio_autopilot():
-    # creating process to fill com_queue with random positions
-    logger.info("Creating center of mass queue...")
-    com_queue = Queue()
-    # filler_proc = Process(target=rand_filler_proc, args=(com_queue, np.array([0, 0]),))
-    # filler_proc.start()
+    """Starts autopilot for all thymio robots defined in the settings.network"""
 
-    # cretaing process to fill thymio_queue with random positions
-    logger.info("Creating thymio queue...")
-    thymio_queue = Queue()
-    # thymio_proc = Process(target=rand_filler_proc, args=(thymio_queue, np.array([2, -2]),))
-    # thymio_proc.start()
+    # creating a set of queues (one for each thymio) with center of mass of the fish school
+    logger.info("Creating center of mass queue(s)...")
+    com_queues = []
+    for thymio in network.thymios.values():
+        com_queue = Queue()
+        com_queues.append(com_queue)
 
-    # start database process that read json files writed db and forwards them to the thymio
+    # creating a set of queues (one for each thymio) with center of mass of the fish school
+    logger.info("Creating thymio queue(s)...")
+    thymio_queues = []
+    for thymio in network.thymios.values():
+        thymio_queue = Queue()
+        thymio_queues.append(thymio_queue)
+
+    # start database process that read json files written db and forwards them to the thymio
     logger.info(f"Starting database daemon consuming {database.database_input_path}...")
 
     # check if database input folder exists
     if not os.path.exists(database.database_input_path):
         raise FileNotFoundError(f"Database input folder {database.database_input_path} does not exist. ")
 
-    # check if input folder is empty
+    # We always wipe the database input folder first (fresh start)
     with_wipe_input = True
 
+    # create the database process that also pushes information into the defined queues for the thymios
     db_process = Process(target=database_daemon_process, args=(database.database_input_path, with_wipe_input,
-                                                               com_queue, thymio_queue,))
+                                                               com_queues, thymio_queues,))
 
-    thmyio_master = CoBeThymioMaster(target_thymio_name="thymio_0")
+    # create a set of thymio masters, these are bridge classes between this computer and the thymios via Pyro5
+    thmyio_masters = [CoBeThymioMaster(target_thymio_name=f"thymio_{i}") for i in range(len(network.thymios))]
 
-    autopilot_process = Process(target=thmyio_master.thymio_autopilot, args=(thymio_queue, com_queue, False,))
+    # create a single process thread for each thymio
+    autopilot_processes = []
+    for i in range(len(network.thymios)):
+        autopilot_process = Process(target=thmyio_masters[i].thymio_autopilot,
+                                    args=(thymio_queues[i], com_queues[i], False,))
+        autopilot_processes.append(autopilot_process)
 
+    # starting the database daemon
     db_process.start()
     logger.info("Database daemon started and passed com and thymio queues.")
-    autopilot_process.start()
-    logger.info("Autopilot started.")
 
-    input("Press enter to stop autopilot...")
+    # starting the autopilot processes
+    for i in range(len(network.thymios)):
+        autopilot_processes[i].start()
+        logger.info(f"Autopilot started on thymio {i}.")
 
+    # wait for user input to stop the autopilot
+    input("Press enter to stop autopilot on all robots...")
+
+    # stopping the autopilot processes and request thymios to be stopped
     logger.info("Stopping autopilot...")
-    autopilot_process.terminate()
-    autopilot_process.join()
-    logger.info("Autopilot stopped.")
+    for i in range(len(network.thymios)):
+        autopilot_processes[i].terminate()
+        autopilot_processes[i].join()
+        logger.info("Autopilot stopped on thymio {i}.")
 
-    logger.info("Stopping thymios")
-    thmyio_master.stop_thymios()
+        logger.info(f"Stopping thymio {i}...")
+        thmyio_masters[i].stop_thymios()
 
+    # stopping the database daemon
     logger.info("Stopping database daemon...")
     db_process.terminate()
     db_process.join()
-    logger.info("Database daemon stopped.")
 
+    logger.info("Database daemon stopped.")
+    logger.info("Autopilot stopped. Bye bye!")
 
 
 def start_thymioserver(th_id=None):
@@ -608,8 +626,9 @@ def start_thymioserver(th_id=None):
     args = argparse.ArgumentParser(description="Starts the pyro5 thymio servers on the chosen thymios")
 
     # adding optional arguments
-    aid = args.add_argument("--thymio_id", default=None, help="ID (int) of the thymio (rpi board) to start thymioserver on as in"
-                                                           "settings.network.thymios")
+    aid = args.add_argument("--thymio_id", default=None,
+                            help="ID (int) of the thymio (rpi board) to start thymioserver on as in"
+                                 "settings.network.thymios")
     args = args.parse_args()
 
     if args.thymio_id is not None:
@@ -731,8 +750,9 @@ def start_thymioserver_silent(th_id=None):
     args = argparse.ArgumentParser(description="Starts the pyro5 thymio servers on the chosen thymios")
 
     # adding optional arguments
-    aid = args.add_argument("--thymio_id", default=None, help="ID (int) of the thymio (rpi board) to start thymioserver on as in"
-                                                           "settings.network.thymios")
+    aid = args.add_argument("--thymio_id", default=None,
+                            help="ID (int) of the thymio (rpi board) to start thymioserver on as in"
+                                 "settings.network.thymios")
     args = args.parse_args()
 
     if args.thymio_id is not None:
@@ -844,5 +864,6 @@ def calibrate(eye_id=-1, on_screen=False):
     if on_screen:
         # generaating calibration image on screen if requested
         master.calibrator.generate_calibration_image(detach=True)
+
     ## Start calibration
     master.calibrate(with_visualization=True, interactive=True, detach=True, eye_id=eye_id)
